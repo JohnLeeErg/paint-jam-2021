41ce74ba18c9ff72eeb960d15aeacdf6876273b4
JohnLeeErg <coojohnl@sheridancollege.ca>
2021-05-01 15:47:12 -0400

    rising from sea

---
+++ Assets/Scripts/RiseFromSeaWhenNearPlayer.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RiseFromSeaWhenNearPlayer : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] float riseSpeed,rotateSpeed, risenZ,radius;
+    Collider2D colRef;
+    float startZ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startZ = transform.position.z;
+        colRef=GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, player.position) < radius)
+        {
+            if (transform.position.z != risenZ)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y, risenZ), riseSpeed * Time.deltaTime);
+            }
+            else
+            {
+                if (!colRef.enabled)
+                {
+                    colRef.enabled = true;
+                }
+                transform.Rotate(transform.up, rotateSpeed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            if (colRef.enabled)
+            {
+                colRef.enabled = false;
+            }
+            if (transform.position.z != startZ)
+            {
+
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y, startZ), riseSpeed * Time.deltaTime);
+            }
+        }
+    }
+}
